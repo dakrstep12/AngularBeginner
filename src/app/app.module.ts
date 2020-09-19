5dba161f8fd9875dccfefb0b3df9137b85012210
dakrstep12 <vanshock5@gmail.com>
2020-09-19 14:44:57 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskInputComponent } from './task-input/task-input.component';
+import { TaskItemComponent } from './task-item/task-item.component';
+import { TaskCheckboxComponent } from './task-checkbox/task-checkbox.component';
+import { TaskService } from './task.service';
+import { TabComponent } from './ui/tab/tab.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent,
+    TaskInputComponent,
+    TaskItemComponent,
+    TaskCheckboxComponent,
+    TabComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [TaskService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
